cab73d270c0b7c025b28d8a272633ccce752a5a6
ZainRasheed <rasheedulla97@gmail.com>
2019-10-08 21:09:09 +0530

    Preprocessed data

--- Uber data/Anakysis.py
+++ Uber data/Anakysis.py
@@ -9,62 +9,69 @@ Created on Sat Oct  5 09:30:12 2019
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+
 
 #Importing the data
 Uber = pd.read_csv("C:/Users/mohammeds2/OneDrive - Verifone/Desktop/New folder (4) ML/Uber data/Uber_Request_Data.csv")
+#Data imported into a dataframe 'Uber'
 
-#Checking for duplicate rows
-#if te max od count is 1, no duplicate
-#if the grapg is not being plotted, duplicate exist
+
+
+#Checking for duplicate rows in RequestID
+#And removing if no duplicates
+''' FAILED or bad strategy
 X = Uber["Request id"]
 X_Count = X.value_counts()
-X_Count_max = X_Count.max()
-plt.plot(X,X_Count)
+X_Count_max = X_Count.max()   #if te max od count is 1, no duplicate
+plt.plot(X,X_Count)     #if the grapg is not being plotted, duplicate exist 
+'''
+X = Uber["Request id"]
+#Copied the column into 'X'
+
+'''X_Count = X.repeat(repeats > n) // doesn't work '''
+'''Another strategy exist using df.describe()'''
+#set true for every deuplicated value
+X_Bool = X.duplicated(keep = False)
+#Result : a series of bool, true for every dulpicated value
+
+#Check if there is any True in the series
+print(any(X_Bool))                     
+# Result : Prints False, Hence no repeatation
 
 #remove the col
 Uber = Uber.iloc[:,1:6]
+#Result : ReqID is sliced from the dataset
 
-# replacing th nan values with 0
-X = Uber["Driver id"].copy()
-X_bool = pd.isnull(X)
-X[X_bool] = 0
-Uber["Driver id"] = X
 
-'''USING COPY TO AVOID WARNING'''
-'''__main__:3: SettingWithCopyWarning: 
-A value is trying to be set on a copy of a slice from a DataFrame'''
 
-X = Uber["Drop timestamp"].copy()
-X_bool = pd.isnull(X)
-X[X_bool] = 0
-Uber["Drop timestamp"] = X
+#Removing the Drop timestamp
+#Removing the Driver id
+#As there is no reqment of that column
+Uber = Uber[["Pickup point","Status","Request timestamp"]]
+#Result : Dataset with "Pickup point","Status","Request timestamp"
+
+
 
 #Removing month, day, year from the time stamps and getting a single format
-#month and day are same
+#month and year are same
 #days is linear
-X = Uber["Drop timestamp"]
-X = pd.to_datetime(X) #converting to numoy datetime type dtype('<M8[ns]')
-X = X.dt.strftime('%S:%M:%H')
-Uber["Drop timestamp"] = X
-
-X = Uber["Request timestamp"]
-X = pd.to_datetime(X) #converting to numoy datetime type dtype('<M8[ns]')
-X = X.dt.strftime('%S:%M:%H')
+X = Uber["Request timestamp"] 
+X = pd.to_datetime(X) #converting to numpy datetime type dtype('<M8[ns]')
+X = X.dt.strftime('%S:%M:%H') #converting the timetsamp to ss:mm:hh
 Uber["Request timestamp"] = X
+#Result : Dataset with converted timestamp ss:mm:hh
 
 """Uni analysis"""
+#Describe the dataset
 Uber.describe()
+#Result : Shows te count,unique values,etc of the datafraem
 Uber["Pickup point"].value_counts()
-Uber["Driver id"].value_counts()
 Uber["Status"].value_counts()
 Uber["Request timestamp"].value_counts()
-Uber["Drop timestamp"].value_counts()
 
-Uber["Pickup point"].hist()
-plt.plot()
-plt.show()
-Uber["Driver id"].hist()
-Uber["Status"].hist()
-Uber["Request timestamp"].hist()
-Uber["Drop timestamp"].hist()
-plt.plot()
+#Uber["Pickup point"].plot.hist(bins=5) // this will not work if the seried or dataframe is not numeric data
+Uber["Pickup point"].apply(pd.value_counts).plot.hist(bins=3)
+Uber["Status"].apply(pd.value_counts).plot.hist(bins=2)
+Uber["Request timestamp"].plot.hist(bins=10)
+Uber["Request timestamp"].apply(pd.value_counts).plot(style = "k.")
