58f8d6f5e522ba095cf7d1bdc149d17c60c3a01b
ZainRasheed <rasheedulla97@gmail.com>
2019-10-17 17:45:54 +0530

    Date changed to discrete values

--- Uber data/Anakysis.py
+++ Uber data/Anakysis.py
@@ -53,25 +53,93 @@ Uber = Uber[["Pickup point","Status","Request timestamp"]]
 
 
 
-#Removing month, day, year from the time stamps and getting a single format
-#month and year are same
-#days is linear
-X = Uber["Request timestamp"] 
+#Removing month, year from the time stamps and getting a single time format
+#month and year are same in every row hance removing it
+#days is moved to a new column 
+X = Uber["Request timestamp"]
+
+#Identifying the indexes from the series for different time formats
+X_Bool_type_m = X.str.contains("/")
+#Getting a index boolean series for dd/mm/yyyy hh:mm format
+X_Bool_type_d = X.str.contains("-")
+#Getting a index boolean series for dd-mm-yyyy hh:mm:ss format
+
 X = pd.to_datetime(X) #converting to numpy datetime type dtype('<M8[ns]')
-X = X.dt.strftime('%S:%M:%H') #converting the timetsamp to ss:mm:hh
+temp_date_col = X.copy() #copying the column to another vairable
+
+#getting the dates out from other both date formats 
+temp_date_col[X_Bool_type_m] = X[X_Bool_type_m].dt.strftime('%m')
+#got the date from the dd/mm/yyyy hh:mm format, but it was placed in the month position
+temp_date_col[X_Bool_type_d] = X[X_Bool_type_d].dt.strftime('%d')
+#got the date from the dd-mm-yyyy hh:mm:ss format, but it was placed in the month position
+
+#adding a new column
+Uber["Request date"] = temp_date_col
+#Request date column added
+
+
+
+ #Segregated column for date
+X = Uber["Request timestamp"]
+X = pd.to_datetime(X)
+X = X.dt.strftime('%H:%M:%S') #converting the timetsamp to ss:mm:hh
 Uber["Request timestamp"] = X
 #Result : Dataset with converted timestamp ss:mm:hh
+
+
+
+#categorizing time
+plt.scatter(Uber["Request time"], Uber["Request date"], s=10, c='b', marker="s", label='first')
+plt.legend(loc='upper left');
+plt.show()
+
+
+
+
+
 
 """Uni analysis"""
 #Describe the dataset
 Uber.describe()
-#Result : Shows te count,unique values,etc of the datafraem
+"""
+Result : Shows te count,unique values,etc of the dataframe
+       Pickup point          Status Request timestamp Request date
+count          6745            6745              6745         6745
+unique            2               3              5618            5
+top            City  Trip Completed    11/7/2016 9:40           15
+freq           3507            2831                 6         1381
+"""
+
+#count of types
 Uber["Pickup point"].value_counts()
+"""
+City       3507
+Airport    3238
+Name: Pickup point, dtype: int64
+"""
+
 Uber["Status"].value_counts()
+"""
+Trip Completed       2831
+No Cars Available    2650
+Cancelled            1264
+Name: Status, dtype: int64
+"""
+
 Uber["Request timestamp"].value_counts()
+"""
+11/7/2016 9:40         6
+11/7/2016 8:37         6
+                      ...
+15-07-2016 19:49:13    1
+14-07-2016 10:06:29    1
+Name: Request timestamp, Length: 5618, dtype: int64
+"""
+#sns.heatmap(Uber["Request timestamp"])
 
 #Uber["Pickup point"].plot.hist(bins=5) // this will not work if the seried or dataframe is not numeric data
-Uber["Pickup point"].apply(pd.value_counts).plot.hist(bins=3)
+#Histogram to see the distributions
+Uber["Pickup point"].apply(pd.value_counts).plot.hist()
 Uber["Status"].apply(pd.value_counts).plot.hist(bins=2)
-Uber["Request timestamp"].plot.hist(bins=10)
-Uber["Request timestamp"].apply(pd.value_counts).plot(style = "k.")
+#Uber["Request timestamp"].plot.hist(bins=10)
+#Uber["Request timestamp"].apply(pd.value_counts).plot(style = "k.")
